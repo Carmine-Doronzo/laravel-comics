6a845fb27d32e88fa5a4a14ee13720b74cc01f9d
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-05-22 15:45:12 +0200

    ADD card image and title

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.app')
 @section('title','comics')
 @section('content')
-
+@dump($comics)
 <h2>comics</h2>
+<div class="row">
+    @foreach($comics as $comic)
+    <div class="col-4">
+       <div class="card">
+        <img src="{{$comic['thumb']}}" alt="">
+        <h1>{{$comic['title']}}</h1>
+       </div>
+    </div>
+    @endforeach
+</div>
 @endsection
